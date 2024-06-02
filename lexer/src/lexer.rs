d4d63de494e5d1d7353e3b3af805a7c4fa5f2a59
Arijit Dasgupta <arijit@dazgupta.com>
2024-06-02 17:05:52 +0200

    minor changes

--- lexer/src/lexer.rs
+++ lexer/src/lexer.rs
@@ -23,7 +23,7 @@ pub(crate) enum Token<'a> {
 fn parse_label_block_greedily<'a>(data: &'a str) -> Result<StringyParseResult, LexxerError> {
     let mut idx: usize = 1;
 
-    while true {
+    loop {
         let d = data.as_bytes().get(idx);
         match d {
             None => return Err(LexxerError::UnterminatedLabelLiteral),
